1d9332bd926437c3a479b563fb2fc78536803bad
ananthunalloor <ananthunalloor@gmail.com>
2025-06-11 09:04:41 +0530

    initial commit, integrations added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import solidJs from '@astrojs/solid-js';
+
+import vue from '@astrojs/vue';
+
+import sitemap from '@astrojs/sitemap';
+
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), solidJs(), vue(), sitemap(), partytown()]
+});
