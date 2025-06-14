1a68cc7b4e3b0baa9d52efa2c85c5179dc2bdd57
ananthunalloor <ananthunalloor@gmail.com>
2025-06-14 11:46:01 +0530

    working confetti

--- src/games/react/tic-tac-toe/tic-tac-toe.tsx
+++ src/games/react/tic-tac-toe/tic-tac-toe.tsx
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useState } from "react";
+import JSConfetti from "js-confetti";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 type BoardType = Map<number, string>;
 
@@ -17,7 +18,9 @@ const winCombinations = [
 ];
 
 export const TicTacToe = () => {
-  const [currentPlayer, setCurrentPlayer] = useState(player1);
+  const jsConfetti = new JSConfetti();
+  const initialPlayer = useRef<string>(Math.random() < 0.5 ? player1 : player2);
+  const [currentPlayer, setCurrentPlayer] = useState(initialPlayer.current);
 
   const [board, setBoard] = useState<BoardType>(new Map<number, string>());
 
@@ -34,7 +37,9 @@ export const TicTacToe = () => {
 
   const resetGame = useCallback(() => {
     setBoard(new Map());
-    setCurrentPlayer(player1);
+    initialPlayer.current =
+      initialPlayer.current === player1 ? player2 : player1;
+    setCurrentPlayer(initialPlayer.current);
   }, [setBoard, setCurrentPlayer]);
 
   const checkWin = useCallback(
@@ -56,6 +61,7 @@ export const TicTacToe = () => {
             );
             return newScores;
           });
+          jsConfetti.addConfetti({});
           resetGame();
         }
       });
@@ -86,16 +92,16 @@ export const TicTacToe = () => {
     <div className="flex flex-col items-center h-full max-w-sm md:max-w-lg mx-auto gap-4">
       <h1 className="text-5xl font-light pt-4">Tic Tac Toe</h1>
       <div className="flex flex-row w-full justify-between items-center">
-        <div className="flex p-4 gap-4 flex-row items-center rounded-md border border-dashed border-gray-400/80 justify-between min-w-48">
-          <div className="flex flex-shrink-0 h-16 w-16 items-center justify-center bg-green-200 text-4xl font-medium md:text-5xl rounded">
+        <div className="flex p-2 gap-4 flex-row items-center rounded-md border border-dashed border-gray-400/80 justify-between min-w-48">
+          <div className="flex flex-shrink-0 h-14 w-14 items-center justify-center bg-green-200 text-4xl font-medium md:text-5xl rounded">
             <p className="text-green-700">{player1}</p>
           </div>
           <div className="flex flex-shrink-0 text-5xl font-light md:text-6xl items-center justify-center">
             {playerScores.get(player1)}
           </div>
         </div>
-        <div className="flex p-4 gap-4 flex-row items-center rounded-md border border-dashed border-gray-400/80 justify-between min-w-48">
-          <div className="flex flex-shrink-0 h-16 w-16 items-center justify-center bg-red-200 text-4xl font-medium md:text-5xl rounded">
+        <div className="flex p-2 gap-4 flex-row items-center rounded-md border border-dashed border-gray-400/80 justify-between min-w-48">
+          <div className="flex flex-shrink-0 h-14 w-14 items-center justify-center bg-red-200 text-4xl font-medium md:text-5xl rounded">
             <p className="text-red-700">{player2}</p>
           </div>
           <div className="flex flex-shrink-0 text-5xl font-light md:text-6xl items-center justify-center">
@@ -103,16 +109,43 @@ export const TicTacToe = () => {
           </div>
         </div>
       </div>
-      <div>Current Player {currentPlayer}</div>
+      <div className="p-2 flex flex-row items-center justify-center text-xl font-light gap-2">
+        <p className="italic">Current Player</p>
+        <p
+          className={`w-8 h-8 flex items-center justify-center rounded ${
+            currentPlayer === player1
+              ? "bg-green-200 text-green-700"
+              : "bg-red-200 text-red-700"
+          }`}
+        >
+          {currentPlayer}
+        </p>
+      </div>
       <div className="rounded-md border border-dashed border-gray-700 w-120 aspect-square">
-        <div className="grid grid-cols-3 grid-rows-3 h-full w-full">
+        <div
+          className={`grid grid-cols-3 grid-rows-3 h-full w-full ${
+            currentPlayer === player1 ? "bg-green-50" : "bg-red-50"
+          }`}
+        >
           {[...Array(9)].map((_, index) => (
             <div
               key={`tic-tac-toe-grid-${index}`}
-              className="flex items-center justify-center border border-gray-500/50 hover:cursor-pointer"
+              className={`flex items-center justify-center border border-gray-500/50 ${
+                board.get(index) !== undefined
+                  ? "hover:cursor-not-allowed"
+                  : "hover:cursor-pointer hover:bg-gray-50/50 "
+              }`}
               onClick={() => handleClick(index)}
             >
-              <p className="text-6xl font-light">{board.get(index)}</p>
+              <p
+                className={`text-6xl text-shadow-md ${
+                  board.get(index) === player1
+                    ? "text-green-400"
+                    : "text-red-400"
+                }`}
+              >
+                {board.get(index)}
+              </p>
             </div>
           ))}
         </div>
